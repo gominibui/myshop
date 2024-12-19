8f9c57a1f96bf7a42eb638bd2d948930ebb45de0
gominibui <164637350+gominibui@users.noreply.github.com>
2024-12-19 21:52:16 +0200

    product list

--- main/models.py
+++ main/models.py
@@ -11,6 +11,9 @@ class Category(models.Model):
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args={self.slug})
+
     def __str__(self):
         return self.name
 
