cd91cceea6ddf47c189bb38b223af187835b49f1
gominibui <164637350+gominibui@users.noreply.github.com>
2024-12-26 18:38:05 +0200

    add cart and some logic

--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
+from cart.forms import CartAddProductForm
 
 def popular_list(request):
     products = Product.objects.filter(available=True)[:3]
@@ -12,22 +14,30 @@ def product_detail(request, slug):
                                 slug=slug,
                                 available=True)
 
+    cart_product_form = CartAddProductForm
+
     return render(request,
                   'main/product/detail.html',
-                  {'product': product})
+                  {'product': product,
+                   'cart_product_form': cart_product_form,})
 
 
 def product_list(request, category_slug=None):
+    page = request.GET.get('page', 1)
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
+    paginator = Paginator(products, 10)
+    current_page = paginator.page(int(page))
     if category_slug:
         category = get_object_or_404(Category,
                                      slug=category_slug)
-        products = products.filter(category=category)
+        paginator = Paginator(products.filter(category=category), 10)
+        current_page = paginator.page(int(page))
     return render(request,
                   'main/product/list.html',{
             'category': category,
-            'catigories': categories,
-            'products': products
+            'categories': categories,
+            'products': current_page,
+            'slug_url': category_slug,
                   })
